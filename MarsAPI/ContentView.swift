d0e89125bc29cb53a6b64799de4a39fcb3d5b2c4
Finn Gilligan <finngilligan0@gmail.com>
2024-12-16 14:57:31 -0500

    Initial Commit

---
+++ MarsAPI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MarsAPI
+//
+//  Created by Finn Gilligan (student LM) on 12/16/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MarsAPI/MarsAPIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MarsAPIApp.swift
+//  MarsAPI
+//
+//  Created by Finn Gilligan (student LM) on 12/16/24.
+//
+
+import SwiftUI
+
+@main
+struct MarsAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
